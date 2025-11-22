427118c1b269548aa4421597ae7e99450a6c4b7b
jhanvi857 <jhanvip8507@gmail.com>
2025-11-22 23:08:14 +0530

    import error fix for mockAPI

--- src/pages/dashboard/DashBoard.jsx
+++ src/pages/dashboard/DashBoard.jsx
@@ -6,7 +6,7 @@ import DashboardSidebar from "../../components/DashBoardSidebar"
 import DashboardTopbar from "../../components/DashBoardTopbar"
 import { Link } from "react-router-dom"
 import { FileText, Download, Trash2, Eye } from "lucide-react"
-import { mockApiLayer } from "../../lib/mockApi"
+import { mockApiLayer } from "../../lib/mockAPI"
 
 export default function Dashboard() {
   const navigate = useNavigate()
