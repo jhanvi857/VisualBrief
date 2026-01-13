ab9d4b95e59b6259b47b22d380a827076af3418a
jhanvi857 <jhanvip8507@gmail.com>
2026-01-13 21:12:46 +0530

    NLTK_setup

--- backend/ML/ML_module.py
+++ backend/ML/ML_module.py
@@ -7,15 +7,15 @@ from collections import Counter
 import string
 from nltk.tokenize.punkt import PunktSentenceTokenizer
 
-try:
-    nltk.data.find("tokenizers/punkt")
-except LookupError:
-    nltk.download("punkt")
+# try:
+#     nltk.data.find("tokenizers/punkt")
+# except LookupError:
+#     nltk.download("punkt")
 
-try:
-    nltk.data.find("corpora/stopwords")
-except LookupError:
-    nltk.download("stopwords")
+# try:
+#     nltk.data.find("corpora/stopwords")
+# except LookupError:
+#     nltk.download("stopwords")
 
 sentence_tokenizer = PunktSentenceTokenizer()
 try:
